2dca20400e5f98f926c48b57ead364842db5c415
An1honyY <ant.jo.yuen@gmail.com>
2024-08-29 23:41:17 +1200

    TinyMCE working html to text

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,14 +6,14 @@ function Home() {
         <div className="hero bg-base-200 min-h-screen justify-content">
           <div className="hero-content flex-col lg:flex-row-reverse m-5">
   
-              <div className="mockup-code">
+              <div className="mockup-code relative bottom-10">
                 <pre data-prefix="1"><code>Welcome! Think of a topic.</code></pre>
                 <pre data-prefix="2"><code>Breathe...</code></pre>
                 <pre data-prefix="3" className="bg-success text-warning-content"><code>Write!</code></pre>
               </div>
   
             <div>
-              <h1 className="text-5xl font-bold">Mini Essays</h1>
+              <h1 className="text-5xl font-bold mt-5">Mini Essays</h1>
               <p className="py-6">
                 A simple writing tool that helps you focus your thoughts.
                 <br></br>
--- frontend/src/pages/NewEssay.tsx
+++ frontend/src/pages/NewEssay.tsx
@@ -3,34 +3,56 @@ import BundledEditor from './components/BundledEditor'
 
 function NewEssay() {
     const editorRef = useRef<any>(null);
-    const log = () => {
+    let editorContent = editorRef.current?.getContent();
+
+    const save = () => {
         if (editorRef.current) {
-            console.log(editorRef.current.getContent());
+            editorContent = editorRef.current?.getContent();
+
+            // Display the editor content in the output div as a preview of the text minus the HTML tags
+            const output = 'output';
+            const outputElement = document.getElementById(output);
+            if (outputElement) {
+                outputElement.innerHTML = editorContent;
+            }
+
+            log();
         }
     };
-        return (
-            <>
-                <h1 className='justify-self-centre'>New Essay</h1>
-                <BundledEditor
-                    onInit={(_evt: any, editor: any) => editorRef.current = editor}
-                    initialValue='<p>This is the initial content of the editor.</p>'
-                    init={{
-                        height: 500,
-                        menubar: false,
-                        plugins: [
-                            'advlist', 'anchor', 'autolink', 'help', 'image', 'link', 'lists',
-                            'searchreplace', 'table', 'wordcount'
-                        ],
-                        toolbar: 'undo redo | blocks | ' +
-                            'bold italic forecolor | alignleft aligncenter ' +
-                            'alignright alignjustify | bullist numlist outdent indent | ' +
-                            'removeformat | help',
-                        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-                    }}
-                />
-                <button onClick={log}>Log editor content</button>
+    const log = () => {
+        if (editorRef.current) {
+            console.log(editorContent);
+        }
+    };
 
-            </>
-        )
-    }
-    export default NewEssay
+    return (
+        <div className='container mx-auto pt-6'>
+            <h1>New Essay</h1>
+
+            <BundledEditor
+                onInit={(_evt: any, editor: any) => editorRef.current = editor}
+                initialValue='<p>...</p>'
+                init={{
+                    selector: 'textarea',  // change this value according to your HTML
+                    height: 500,
+                    menubar: false,
+                    plugins: [
+                        'advlist', 'anchor', 'autolink', 'help', 'image', 'link', 'lists',
+                        'searchreplace', 'table', 'wordcount'
+                    ],
+                    toolbar: 'undo redo | blocks | ' +
+                        'bold italic forecolor | alignleft aligncenter ' +
+                        'alignright alignjustify | bullist numlist outdent indent | ' +
+                        'removeformat | help',
+                    content_style: 'body { font-family:Arial,Helvetica,sans-serif; font-size:14px }',
+                    skin: 'oxide-dark',
+                    content_css: 'dark',
+                }}
+            />
+            <button className='btn bg-primary m-6 font-bold text-primary-content' onClick={save}>Save</button>
+                <div id="output" className='border'></div>
+
+        </div>
+    )
+}
+export default NewEssay
