bd2e95279c73fc1f66d7ae52fe291dca76afb6ed
An1honyY <ant.jo.yuen@gmail.com>
2024-08-30 00:45:45 +1200

    Text Editor changed to BlockNote from TinyMCE

--- frontend/src/pages/NewEssay.tsx
+++ frontend/src/pages/NewEssay.tsx
@@ -1,58 +1,69 @@
-import { useRef } from 'react';
-import BundledEditor from './components/BundledEditor'
+import { Block, BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from '@blocknote/mantine';
+import "@blocknote/mantine/style.css";
+
+import { useEffect, useMemo, useState } from "react";
+
+let debounceTimer: number;
+
+async function saveToStorage(jsonBlocks: Block[]) {
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => {
+        // Maybe replace with a call to your API / database.
+        localStorage.setItem("editorContent", JSON.stringify(jsonBlocks));
+    }, 300);
+}
+
+async function loadFromStorage() {
+    // Gets the previously stored editor contents.
+    const storageString = localStorage.getItem("editorContent");
+    return storageString
+        ? (JSON.parse(storageString) as PartialBlock[])
+        : undefined;
+}
+
 
 function NewEssay() {
-    const editorRef = useRef<any>(null);
-    let editorContent = editorRef.current?.getContent();
+    // For more on using BlockNote, see https://www.blocknotejs.org/docs/editor-basics/setup
+    const [initialContent, setInitialContent] = useState<
+        PartialBlock[] | undefined | "loading"
+    >("loading");
 
-    const save = () => {
-        if (editorRef.current) {
-            editorContent = editorRef.current?.getContent();
+    // Loads the previously stored editor contents.
+    useEffect(() => {
+        loadFromStorage().then((content) => {
+            setInitialContent(content);
+        });
+    }, []);
 
-            // Display the editor content in the output div as a preview of the text minus the HTML tags
-            const output = 'output';
-            const outputElement = document.getElementById(output);
-            if (outputElement) {
-                outputElement.innerHTML = editorContent;
-            }
-
-            log();
+    // Creates a new editor instance.
+    // We use useMemo + createBlockNoteEditor instead of useCreateBlockNote so we
+    // can delay the creation of the editor until the initial content is loaded.
+    const editor = useMemo(() => {
+        if (initialContent === "loading") {
+            return undefined;
         }
-    };
-    const log = () => {
-        if (editorRef.current) {
-            console.log(editorContent);
-        }
-    };
+        return BlockNoteEditor.create({ initialContent });
+    }, [initialContent]);
 
+    if (editor === undefined) {
+        return "Loading content...";
+    }
     return (
-        <div className='container mx-auto pt-6'>
+
+        <>
+            <div className='container mx-auto pt-6'>
             <h1>New Essay</h1>
+                <BlockNoteView
+                    editor={editor}
+                    onChange={() => {
+                        saveToStorage(editor.document);
+                    }}
+                />
+            </div>
+        </>
 
-            <BundledEditor
-                onInit={(_evt: any, editor: any) => editorRef.current = editor}
-                initialValue='<p>...</p>'
-                init={{
-                    selector: 'textarea',  // change this value according to your HTML
-                    height: 500,
-                    menubar: false,
-                    plugins: [
-                        'advlist', 'anchor', 'autolink', 'help', 'image', 'link', 'lists',
-                        'searchreplace', 'table', 'wordcount'
-                    ],
-                    toolbar: 'undo redo | blocks | ' +
-                        'bold italic forecolor | alignleft aligncenter ' +
-                        'alignright alignjustify | bullist numlist outdent indent | ' +
-                        'removeformat | help',
-                    content_style: 'body { font-family:Arial,Helvetica,sans-serif; font-size:14px }',
-                    skin: 'oxide-dark',
-                    content_css: 'dark',
-                }}
-            />
-            <button className='btn bg-primary m-6 font-bold text-primary-content' onClick={save}>Save</button>
-                <div id="output" className='border'></div>
-
-        </div>
     )
 }
 export default NewEssay
